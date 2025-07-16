04319ff5b46361eee81c9054c2e2cc1f51bb32fc
electroem3000 <em_vera@mail.ru>
2025-07-16 22:55:33 +0300

    expand console menu

--- ui/src/main/java/io/proj3ct/ConsoleUI.java
+++ ui/src/main/java/io/proj3ct/ConsoleUI.java
@@ -1,5 +1,6 @@
 package io.proj3ct;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleUI {
@@ -13,21 +14,22 @@ public class ConsoleUI {
     }
 
     public void start() {
-        // Загрузка заданий из файла
         manager.getTasks().addAll(storage.load());
         while (true) {
-            System.out.println("\nTodo List:");
-            for (Task t : manager.getTasks()) {
-                System.out.println(t);
-            }
             System.out.println("\nMenu:" +
                     "\n1) Add" +
                     "\n2) Complete" +
                     "\n3) Remove" +
-                    "\n4) Save & Exit");
+                    "\n4) Show list" +
+                    "\n5) Show pending" +
+                    "\n0) Save & Exit");
             System.out.print("Choose: ");
             String input = scanner.nextLine();
             switch (input) {
+                case "0":
+                    storage.save(manager.getTasks());
+                    System.out.println("Saved. Goodbye!");
+                    return;
                 case "1":
                     System.out.print("Enter description: ");
                     String desc = scanner.nextLine();
@@ -44,9 +46,28 @@ public class ConsoleUI {
                     if (!manager.removeTask(rid)) System.out.println("Not found!");
                     break;
                 case "4":
-                    storage.save(manager.getTasks());
-                    System.out.println("Saved. Goodbye!");
-                    return;
+                    System.out.println("\nFull Todo List:");
+                    for (Task t : manager.getTasks()) {
+                        System.out.println(t);
+                    }
+                    break;
+                case "5":
+                    List<Task> tasks = manager.getTasks()
+                            .stream()
+                            .filter(task -> !task.isCompleted())
+                            .toList();
+                    System.out.println("Pending tasks:");
+                    if (tasks.isEmpty()) {
+                        System.out.println("  (список пуст)");
+                    } else {
+                        for (Task t : tasks) {
+                            System.out.printf("  [%d] %-20s",
+                                    t.getId(),
+                                    t.getDescription());
+                        }
+                    }
+                    System.out.println();
+                    break;
                 default:
                     System.out.println("Invalid option.");
             }
